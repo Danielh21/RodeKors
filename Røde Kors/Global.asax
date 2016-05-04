c4489b8501881433b8c30e0b5eafa9a6d03fed65
Danielh21 <danielhollmann@hotmail.com>
2016-05-04 20:00:31 +0200

    Start

---
+++ Røde Kors/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Røde_Kors.MvcApplication" Language="C#" %>
---
+++ Røde Kors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Røde_Kors.Startup))]
+namespace Røde_Kors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
