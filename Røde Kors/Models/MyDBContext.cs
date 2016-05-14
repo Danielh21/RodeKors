4d0e1c025d9e2ec4debe61e88d9269420b879e3c
Danielh21 <danielhollmann@hotmail.com>
2016-05-14 10:37:55 +0200

    Mirgrations to Auto added

---
+++ Røde Kors/Models/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Røde_Kors.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext() : base("Vagtmanager")
+        {
+
+        }
+
+
+    }
+}
---
+++ Røde Kors/Models/MyUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Røde_Kors.Models
+{
+    public class MyUser: IdentityUser
+    {
+        [Required]
+        public int userID { get; set; }
+
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int CPR { get; set; }
+        public string CO { get; set; }
+        public string streetAndNumber { get; set; }
+        public int zipcode { get; set; }
+        public string city { get; set; }
+        public int telefon1 { get; set; }
+        public int telefon2 { get; set; }
+        public string  email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+
+
+        // Look here:  Down low
+        // https://blogs.msdn.microsoft.com/webdev/2013/10/16/customizing-profile-information-in-asp-net-identity-in-vs-2013-templates/
+
+    }
+}
