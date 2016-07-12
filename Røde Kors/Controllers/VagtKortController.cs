b2e78836d83645e11ec234ff9fd81c8c0fecec11
Danielh21 <danielhollmann@hotmail.com>
2016-07-12 17:07:59 +0200

    Creating of VagtKort. Now onto assigning users

--- Røde Kors/Controllers/VagtKortController.cs
+++ Røde Kors/Controllers/VagtKortController.cs
@@ -1,6 +1,7 @@
 ﻿using Røde_Kors.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -55,6 +56,14 @@ namespace Røde_Kors.Controllers
 
         public ActionResult NewVagtkortStart()
         {
+            if(Session["VagtKort"] != null)
+            {
+                VagtKort kort = (VagtKort) Session["VagtKort"];
+                return View(kort);
+            }
+
+            else
+
             return View();
         }
 
@@ -75,17 +84,144 @@ namespace Røde_Kors.Controllers
 
         // POST: Method for handling post Request from the view. 
         [HttpPost]
-        public ActionResult NewVagtKortStart(VagtKort kort)
+        public ActionResult NewVagtKortStart(VagtKort model)
         {
 
-           return RedirectToAction("NewVagtKort2", "Vagtkort");
+            // Make sure that the fields are filled by the user!
+            if ( model.VagtNavn == null ||
+                model.VagtStart == null ||
+                model.ArregmenetSlut == null ||
+                model.VagtSted == null ||
+                model.RekvirentKontaktPerson == null ||
+                model.RekvirentKontaktPersonDetajler == null ||
+                model.VagtAdresse == null ||
+                model.VagtPostnumberBy == null ||
+                model.Rekvirent_RekvirentId == 0
+                )
+            {
+                return View(model);
+            }
+            
+            // Find the Rekvirent and saves it as an object under vagtkort
+            // Not saved in the database!
+            var hash = context.Rekvirents.ToDictionary(t => t.revkirentId);
+            Rekvirent rekvirent = hash[model.Rekvirent_RekvirentId];
+            model.Rekvirent = rekvirent;
+
+            Session["VagtKort"] = model;
+            return RedirectToAction("NewVagtKortDetaljer", "Vagtkort");
         }
 
         // GET: Method for showing second Screen in create vagtkort
 
-        public ActionResult NewVagtKort2()
+        public ActionResult NewVagtKortDetaljer()
         {
-            return View();
+            if (Session["VagtKort"] == null)
+            {
+                return RedirectToAction("NewVagtkortStart", "Vagtkort");
+            }
+            VagtKort model = (VagtKort)Session["VagtKort"];
+            return View(model);
         }
+
+        //POST:
+        // Method for loading the Rekvirent for Vagtkort and sending back a JSON object!
+        public ActionResult ajaxRekvirentStandard()
+        {
+            VagtKort currentVagtKort = (VagtKort) Session["VagtKort"];
+            Rekvirent currentRekivirent = currentVagtKort.Rekvirent;
+            var jsonObj = new JavaScriptSerializer().Serialize(currentRekivirent);
+            return Json(jsonObj);
+
+        }
+
+        // POST: For handling the Details of the VagtKort
+        [HttpPost]
+        public ActionResult NewVagtKortDetaljer(VagtKort model)
+        {
+            if(model.Kommentar == null ||
+                model.Forplejning == null ||
+                model.RødeKorsMaterialer == null ||
+                model.RekvirentMaterialer == null)
+            {
+                return View(model);
+            }
+
+            else
+            {
+                VagtKort vagtkort = (VagtKort) Session["VagtKort"];
+                vagtkort.Kommentar = model.Kommentar;
+                vagtkort.Forplejning = model.Forplejning;
+                vagtkort.RødeKorsMaterialer = model.RødeKorsMaterialer;
+                vagtkort.RekvirentMaterialer = model.RekvirentMaterialer;
+                vagtkort.AntalVagtleder = model.AntalVagtleder;
+                vagtkort.AntalMedics = model.AntalMedics;
+                vagtkort.AntalTeamLeder = model.AntalTeamLeder;
+                vagtkort.AntalTeamSamarit = model.AntalTeamSamarit;
+                vagtkort.AntalElev = model.AntalElev;
+                vagtkort.AntalObs = model.AntalObs;
+                Session["VagtKort"] = vagtkort;
+                return RedirectToAction("ConfirmVagtkort", "VagtKort");
+            }
+        }
+
+            //GET: Confirm VagtKort
+
+            public ActionResult ConfirmVagtkort()
+        {
+            var vagtkort = Session["Vagtkort"];
+            return View(vagtkort);
+        }
+
+
+
+        //GET: Returns an list of VagtKort where the end date has not yet been reached.
+        public ActionResult UpcommingVagtKorts()
+        {
+            List<VagtKort> listofVagtkort = context.VagtKorts.ToList<VagtKort>();
+            List<VagtKort> Upcomming = new List<VagtKort>();
+
+            foreach (VagtKort vagtKort in listofVagtkort)
+            {
+                DateTime now = DateTime.Now;
+
+                if (vagtKort.VagtSlut > now)
+                {
+                    Upcomming.Add(vagtKort);
+                }
+            }
+
+            return View(Upcomming);
+        }
+
+        //POST:
+        // Saves the VagtKort when the user has confirmed it!
+        public ActionResult CreateVagtKort()
+        {
+            VagtKort vagtkort = (VagtKort) Session["Vagtkort"];
+            // Saves the Rekvirent again, and that needs to change!
+            context.Entry(vagtkort.Rekvirent).State = EntityState.Detached;
+            vagtkort = context.VagtKorts.Add(vagtkort);
+            context.SaveChanges();
+
+            Session["Vagtkort"] = null; // Clears the session Vagtkort after the save.
+            // Here!!!! Must return too assigning the ApplicationUsers!
+
+            return RedirectToAction("AssignUsers","VagtKort", new { id = vagtkort.VagtKortId });
+        }
+
+        // GET:
+        // Assign Users to Vagten.
+        // At this point the Vagtkort has been created an assigned an ID in the database!
+        public ActionResult AssignUsers(int id)
+        {
+            //VagtKort vagtkort = context.VagtKorts.Find(id);
+            //List<ApplicationUser> list = context.Users.ToList<ApplicationUser>();
+            //ApplicationUser user = list[0];
+            //context.SaveChanges();
+            //return View(vagtkort);
+        }
+
     }
+
 }
